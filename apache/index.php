98bd9c516a0614f5f30289b17d18cba23099cc84
John Titor <johntitor@JohnnoMacBook-Pro.local>
2024-05-05 15:41:11 +0900

    f

--- apache/index.php
+++ apache/index.php
@@ -8,9 +8,9 @@
 
 <body align="center">
 <form method="post" action="post.php">
-	<input type="number" name="hoge" min="1" max="300">
-	<input type="number" name="moge" min="1" max="300">
-	<input type="submit" value="送信"></br>
+  <input type="number" name="hoge" min="1" max="300">
+  <input type="number" name="moge" min="1" max="300">
+  <input type="submit" value="送信"></br>
   <a href="./list.php">一覧</a>
 </form>
 </body>
--- apache/list.php
+++ apache/list.php
@@ -17,12 +17,12 @@
 <?php
 $csv = file('./hoge.csv');
 foreach ($csv as $line){
-	$hoge = explode(',', $line);
-	echo "<tr>"
-		. "<td>$hoge[0]</td>"
-		. "<td>$hoge[1]</td>"
-		. "<td>$hoge[2]</td>"
-		. "</tr>";
+  $hoge = explode(',', $line);
+  echo "<tr>"
+  . "<td>$hoge[0]</td>"
+  . "<td>$hoge[1]</td>"
+  . "<td>$hoge[2]</td>"
+  . "</tr>";
 }
 ?>
 </body>
